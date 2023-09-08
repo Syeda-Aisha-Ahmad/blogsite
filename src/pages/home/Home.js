46984902b468049a71703c2906c11f096e5c3a4b
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-09-09 00:58:53 +0600

    banner

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Shared/navbar/Navbar';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <Navbar/>
+            <Navbar />
+            <Banner/>
         </div>
     );
 };
